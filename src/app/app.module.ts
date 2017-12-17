89820e5e3d6b9100883e273fa69be5af2643e95a
Joey <jrlegrand@gmail.com>
2017-12-17 08:40:21 -0600

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SigComponent } from './sig/sig.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigComponent
   ],
   imports: [
     BrowserModule
